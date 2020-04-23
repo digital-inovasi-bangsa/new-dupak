ce35de281f56f3e0061fc83f007853af53043c14
David Rigan <davidrigan.dev@gmail.com>
2020-04-24 01:44:07 +0700

    #18

---
+++ application/views/includes/auth/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title><?= $pageTitle; ?> | DUPAK Basarnas</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <link href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="login-page skin-yellow" style="background-image: url('<?=base_url();?>assets/images/basarnas.webp'); background-size: cover;">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="/"><img src="<?=base_url();?>assets/images/logo.png" alt="Dupak Basarnas Yogyakarta" width="150"></a>
+      </div><!-- /.login-logo -->
