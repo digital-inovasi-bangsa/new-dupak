fff985fc1f8a98ba9133da14547feaa30a064569
David Rigan <davidrigan.dev@gmail.com>
2020-04-26 14:48:58 +0700

    impove table width

--- application/views/butir_kegiatan/butir_kegiatan.php
+++ application/views/butir_kegiatan/butir_kegiatan.php
@@ -12,7 +12,7 @@
     </div>
 
     <div class="row">
-      <div class="col-xs-12 col-md-6">
+      <div class="col-xs-12 col-md-12">
         <div class="box">
           <div class="box-header">
             <h3 class="box-title"><?= $pageTitle; ?></h3>
